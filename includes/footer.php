cf8fc79b3ddf88f423f6bfeafe8fe6c8b26c164d
Ranjan Kumar Singh <ranjankumar02feb@gmail.com>
2025-04-02 18:41:42 +0530

    footer and signin page code complate

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<!-- footer section start -->
+ <div class="copy-right">
+    <div class="container">
+        <div class="footer-social wown fedeInDown animated animated" data-wow-delay=".5s" style="visibility: visibile; animation-delay: 0.5s; animation-name: fadeInDown;">
+            <ul>
+                <li><a class="facebook" href=""><span>Facebook</span></a></li>
+                <li><a class="twitter" href=""><span>Twitter</span></a></li>
+                <li><a class="instagram" href=""><span>Instagram</span></a></li>
+                <li><a class="linkdin" href=""><span>Linkdin</span></a></li>
+            </ul>
+        </div>
+        <p class="wow zoomIn animated animated data-wow-delay=".5s" style="visibility: visibile; animation-delay: 0.5s; animation-name: zoomIn;">@ Copyright TMS. All Right Reserved</p>
+    </div>
+ </div>
---
+++ includes/signin.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(isset($_POST['signin']))
+{
+    $email=$_POST['email'];
+    $password=md5($_POST['password']);
+    $sql = "SELECT EmailId,Password FROM tbblusers WHERE EmailId=:enmail and Password=:password";
+    $query= $dbh -> prepare($sql);
+    $query-> bindParam(':email', $email, PDO::PARAM_STR);
+    $query-> bindParam(':password', $password, PDO::PARAM_STR);
+    $query-> execute();
+    $results=$query->fetchAll(PDO::FETCH_OBJ);
+    if($query->rowCount()> 0)
+    {
+        $_SESSION['login']=$_POST['email'];
+        echo "<script type='text/javascript'>document.location = 'packagwe-list.php'; </script>
+    } else{
+     echo "<script>alert('Invalid DEtails');</script>";
+     }
+}
+?>    
